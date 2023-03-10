85de95472025ca713fd8957ed1158a4fa2cb2af9
kostyasimanov <kostyasimanov@gmail.com>
2023-03-10 21:45:09 +0300

    Add DigitalOcean+AWS repos

--- resources.tf
+++ resources.tf
@@ -1,5 +1,7 @@
 # Add a project owned by the user
 
+# GitLab repos
+
 resource "gitlab_project" "add_gitlab_project" {
   name                   = "Add new gitlab project"
   description            = "Project to add new project at gitlab"
@@ -8,11 +10,32 @@ resource "gitlab_project" "add_gitlab_project" {
   path                   = "add-gitlab-project"
 }
 
+
+resource "gitlab_project" "do-vps-aws-dns" {
+  name                   = "DO VPS and AWS DNS"
+  description            = "Add DigitalOcean VPS + AWS Route53 DNS"
+  default_branch         = "main"
+  initialize_with_readme = true
+  path                   = "dp-vps-aws-dns"
+}
+
+
+# GitHub repos
+
 resource "github_repository" "add_github_repo" {
   name         = "add-new-github-project"
   description  = "Project to add new repo at github"
   auto_init    = true
 }
+
+resource "github_repository" "do-vps-aws-dns" {
+  name         = "do-vps-aws-dns"
+  description  = "Add DigitalOcean VPS + AWS Route53 DNS"
+  auto_init    = true
+}
+
+
+# Deploy Key
 
 resource "gitlab_deploy_key" "personal_gitlab_key" {
   project  = gitlab_project.add_gitlab_project.id
